496bf7ce5490f326982d4be8194b2a256bdb54fe
thisistanzeela <86124816+thisistanzeela@users.noreply.github.com>
2023-04-15 22:01:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,13 @@ class App extends Component {
         return(
             <div id="main">
                {/* Do not remove the main div */}
+            <ol key="relativeList">
+                  <li key="relativeListItem1">Amit</li>
+                  <li key="relativeListItem2">Bhavna</li>
+                  <li key="relativeListItem3">Chandan</li>
+                  <li key="relativeListItem4">Divya</li>
+                  <li key="relativeListItem5">Esha</li>
+            </ol>
             </div>
         )
     }
